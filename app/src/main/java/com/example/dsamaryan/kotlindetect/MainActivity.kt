4cec6965d82c8329b197b3ba63cdae4fe6c771e1
Dmitriy Samaryan <d.samaryan@redmadrobot.com>
2017-07-10 08:21:32 +0300

    working snapshot

--- app/src/main/java/com/example/dsamaryan/kotlindetect/MainActivity.kt
+++ app/src/main/java/com/example/dsamaryan/kotlindetect/MainActivity.kt
@@ -2,11 +2,62 @@ package com.example.dsamaryan.kotlindetect
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val pi = 3.14f
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        System.gc()
+    }
+
+    fun init(
+        v1: Int,
+        v2: Int,
+        v3: Int,
+        v4: Int,
+        v5: Int,
+        v6: Int,
+        v7: Int,
+        v8: Int,
+        v9: Int,
+        v10: Int,
+        v11: Int,
+        v12: Int,
+        v13: Int,
+        v14: Int,
+        v15: Int,
+        v16: Int,
+        pet: Pet
+    ) {
+        val v=v1-v2*v3
+        when(pet) {
+            Pet.DOG -> Log.d("","")
+            Pet.CAT -> Log.d("","")
+            Pet.CAT -> Log.d("","")
+        }
+    }
+    abstract class A {
+        open protected val v = ""
+        open suspend internal fun f(v: Any): Any = ""
+        lateinit public var lv: String
+        tailrec abstract fun findFixPoint(x: Double = 1.0): Double
+    }
+
+    class B : A() {
+        override public val v = ""
+        override suspend fun f(v: Any): Any = ""
+        override tailrec fun findFixPoint(x: Double): Double
+            = if (x == Math.cos(x)) x else findFixPoint(Math.cos(x))
+    }
+
+    enum class Pet {
+        DOG,
+        CAT
     }
 }
---
+++ app/src/main/java/com/example/dsamaryan/kotlindetect/Second.kt
@@ -0,0 +1,9 @@
+package com.example.dsamaryan.kotlindetect
+
+/**
+ * @author d.samaryan
+ */
+object Second {
+
+    const val e = 2.17
+}
